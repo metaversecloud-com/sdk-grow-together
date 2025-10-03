4bc6e9a9a9568236070f3c5f44ba0193398f499f
Lina <linabell@mail.com>
2025-10-03 09:48:56 -0700

    update reserved squares

--- shared/constants/plotConfig.ts
+++ shared/constants/plotConfig.ts
@@ -7,7 +7,7 @@ export const s3URL = "https://topia-dev-test.s3.us-east-1.amazonaws.com/bounty";
 export const plotConfig = {
   gridCols: 4,
   gridRows: 4,
-  reservedSquares: [1, 8, 15], // reserved for decorations
+  reservedSquares: [12, 13, 14, 15], // reserved for decorations
   squareSpacing: 100,
   plotDimensions: {
     width: 400,
